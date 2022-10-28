d5401b0e12f393b6f56d5bcc3577304eed54246f
sharunspi <sharunkk1997@gmail.com>
2022-10-28 07:40:20 +0530

    solved exercise 4.2

--- Week-4/Exercise_4.2/Teacher.js
+++ Week-4/Exercise_4.2/Teacher.js
@@ -4,12 +4,35 @@ Person.prototype.initialize = function (name, age) {
   this.age = age;
 };
 
-class Teacher extends Person {
-  teach(subject) {
-    console.log(`${this.name} is now teaching ${subject}`);
-  }
-}
+// The solution
 
+// .......................
+
+//  --- Method 1 ---
+
+// This is a simple method os ES6 using class and inheritance
+
+// class Teacher extends Person {
+//   teach(subject) {
+//     console.log(`${this.name} is now teaching ${subject}`);
+//   }
+// }
+
+// ................
+
+//  --- Method 2 ---
+// Teacher class is created
+function Teacher() {}
+
+// Assign the prototpes of Person to Teacher
+Object.setPrototypeOf(Teacher.prototype, Person.prototype);
+
+// set new method teach to teacher
+Teacher.prototype.teach = function (subject) {
+  console.log(`${this.name} is now teaching ${subject}`);
+};
+
+// creating new teacher with Teacher constructor
 var him = new Teacher();
 
 him.initialize("Sharun", 25);
