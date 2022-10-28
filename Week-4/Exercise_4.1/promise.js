142133b9e2b248a31b3c24c44202afff9ddcaa57
sharunspi <sharunkk1997@gmail.com>
2022-10-28 22:07:20 +0530

    Aded promise

--- Week-4/Exercise_4.1/promise.js
+++ Week-4/Exercise_4.1/promise.js
@@ -1,3 +1,46 @@
-function getNumber() {}
+function getNumber() {
+  return (Math.random() * 100).toFixed(0);
+}
 
-function Promise(resolve, reject, fulfilled) {}
+function SPromise(func) {
+  let onReject, onResolve;
+  let isCalledCallback = false;
+
+  this.then = function (thenFunc) {
+    onResolve = thenFunc;
+    return this;
+  };
+
+  this.catch = function (catchFunc) {
+    onReject = catchFunc;
+    return this;
+  };
+
+  function resolve(val) {
+    if (typeof onResolve === "function" && !isCalledCallback) {
+      onResolve(val);
+      isCalledCallback = true;
+    }
+  }
+
+  function reject(err) {
+    if (typeof onReject === "function" && !isCalledCallback) {
+      onReject(err);
+      isCalledCallback = true;
+    }
+  }
+
+  func(resolve, reject);
+}
+
+const myPromise = new SPromise((resolve, reject) => {
+  if (getNumber() % 5 === 0) {
+    reject("rejected");
+  } else {
+    resolve("reolved");
+  }
+});
+
+myPromise
+  .then((res) => console.log("number is not divisible by 5"))
+  .catch((err) => console.log("number is divisible by 5"));
