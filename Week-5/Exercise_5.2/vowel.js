bdd82c9f76768aba653e260ad672304bf7e3dcb3
sharunspi <sharunkk1997@gmail.com>
2022-11-06 22:01:19 +0530

    completed second task

--- Week-5/Exercise_5.2/vowel.js
+++ Week-5/Exercise_5.2/vowel.js
@@ -1,4 +1,6 @@
+// to cocunt vowels in string
 function vowelCount(str) {
+  // check is it string
   if (typeof str === "string") {
     const vMap = new Map();
     for (const ele of str) {
@@ -7,15 +9,20 @@ function vowelCount(str) {
         if (vMap.has(str)) {
           vMap.set(str, vMap.get(str) + 1);
         } else {
-          vMap.set();
+          vMap.set(str, 1);
         }
       }
     }
+    return vMap;
   } else {
     throw new Error("Input is not a string");
   }
 }
 
+// vowel finder
 function isVowel(ele) {
   return "aeiou".includes(ele);
 }
+
+let newmap = vowelCount("teacher");
+console.log(newmap);
