47f7e97dffd61010da3177137632f4617164189b
sharunspi <sharunkk1997@gmail.com>
2022-11-06 21:57:27 +0530

    completed task 1

--- Week-5/Exercise_5.1/generate.js
+++ Week-5/Exercise_5.1/generate.js
@@ -1,7 +1,47 @@
-function doTask1() {}
+// 3 sample tasks
+function doTask1() {
+  setTimeout(() => {
+    console.log("first task done");
+  }, 1000);
+}
 
-function doTask2() {}
+function doTask2() {
+  console.log("second task");
+}
 
-function doTask3() {}
+function doTask3() {
+  setTimeout(() => {
+    console.log("third task done");
+  }, 2000);
+}
 
-function asyncAwait() {}
+// async await polyfill
+
+function asyncAwait(gene) {
+  return function () {
+    let func = gene instanceof Function ? gene.apply(this) : gene;
+    function make(result) {
+      if (result.done) {
+        return Promise.resolve(result.value);
+      }
+      return Promise.resolve(result.value)
+        .then((res) => {
+          return make(func.next(res));
+        })
+        .catch((err) => {
+          return make(func.throw(err));
+        });
+    }
+    try {
+      return make(func.next());
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
+}
+
+asyncAwait(function* () {
+  let res1 = yield doTask1();
+  let res2 = yield doTask2();
+  let res3 = yield doTask3();
+})();
