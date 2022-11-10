3ffbe8b5e2a8ac8777f831aff5b1d4c3ef76f5b9
sharunspi <sharunkk1997@gmail.com>
2022-11-10 23:39:20 +0530

    comleted task 1

---
+++ Week-6/Problem_6.1/maxSum.js
@@ -0,0 +1,19 @@
+function maxSumSubArray(arg) {
+  let length = arg.length;
+  //   initializations;
+  let maximum = 0,
+    maximumEnd = 0;
+  for (let i = 0; i <= length; i++) {
+    maximumEnd = maximumEnd + arg[i];
+    // checking whether maximum value is less than end value
+    if (maximum < maximumEnd) {
+      maximum = maximumEnd;
+    }
+    if (maximumEnd < 0) {
+      maximumEnd = 0;
+    }
+  }
+  return maximum;
+}
+
+console.log(maxSumSubArray([1, 2, 4, -5, 4, 1, 455, 45]));
