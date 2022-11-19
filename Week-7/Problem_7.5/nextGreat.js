1576bb7779f570f4138fe08c709834c0308b9ecd
sharunspi <sharunkk1997@gmail.com>
2022-11-20 00:39:19 +0530

    completed 5

---
+++ Week-7/Problem_7.5/nextGreat.js
@@ -0,0 +1,17 @@
+function nextGreaterElement(array, n) {
+  var next, i, j;
+  for (i = 0; i < n; i++) {
+    next = -1;
+    for (j = i + 1; j < n; j++) {
+      if (array[i] < array[j]) {
+        next = array[j];
+        break;
+      }
+    }
+    console.log(array[i] + " -- " + next);
+  }
+}
+
+var newArray = [11, 13, 21, 3];
+var n = newArray.length;
+nextGreaterElement(newArray, n);
