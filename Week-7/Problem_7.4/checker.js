5874b8e5a8743d022379852c90c961a12e27886e
sharunspi <sharunkk1997@gmail.com>
2022-11-20 00:34:01 +0530

    solved task 4

---
+++ Week-7/Problem_7.4/checker.js
@@ -0,0 +1,40 @@
+function checker(exprsion) {
+  // stack
+  let stack = [];
+  let length = exprsion.length;
+
+  for (let i = 0; i < length; i++) {
+    let ele = exprsion[i];
+
+    if (ele == "[" || ele == "(" || ele == "{") {
+      stack.push(ele);
+      continue;
+    }
+    if (stack.length === 0) {
+      return false;
+    }
+
+    let check;
+    switch (ele) {
+      case ")":
+        check = stack.pop();
+        if (check == "{" || check == "[") return false;
+        break;
+      case "]":
+        check = stack.pop();
+        if (check == "(" || check == "{") return false;
+        break;
+      case "}":
+        check = stack.pop();
+        if (check == "(" || check == "[") return false;
+        break;
+    }
+  }
+
+  return stack.length == 0;
+}
+
+// input
+let input = "([{}])";
+
+console.log(" Output :", checker(input));
