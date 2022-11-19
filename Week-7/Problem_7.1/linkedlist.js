748fb9a4f614c7576d9956d2687c630f0cb4656c
sharunspi <sharunkk1997@gmail.com>
2022-11-19 19:24:34 +0530

    solved first question

---
+++ Week-7/Problem_7.1/linkedlist.js
@@ -0,0 +1,39 @@
+let head;
+// for printing list
+function printLinkeList(node) {
+  while (node != null) {
+    console.log(node.value + "->");
+    node = node.next;
+  }
+}
+// single node template
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+// reverse
+function reverse(node) {
+  var prev = null;
+  var current = node;
+  var next = null;
+  while (current != null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  node = prev;
+  return node;
+}
+// data creation
+head = new Node(85);
+head.next = new Node(15);
+head.next.next = new Node(4);
+head.next.next.next = new Node(20);
+
+console.log("Normal list");
+printLinkeList(head);
+
+head = reverse(head);
+console.log("Reversed list");
+printLinkeList(head);
