1f001131619c44536ce35b0be7819c3bf843bd4d
sharunspi <sharunkk1997@gmail.com>
2022-12-17 11:00:12 +0530

    created files

--- Week-10/url-shortner/src/App.js
+++ Week-10/url-shortner/src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
+import Header from "./Components/Header";
+import Banner from "./Components/Banner";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="main">
+        <Banner />
+      </div>
+    </>
   );
 }
 
---
+++ Week-10/url-shortner/src/Components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function index() {
+  return (
+    <header>
+      <span className="brand">Shortly</span>
+      <div className="navigation">
+        <nav></nav>
+      </div>
+      <div className="login"></div>
+    </header>
+  );
+}
+
+export default index;
